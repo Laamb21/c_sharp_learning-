cdbf3547647dba3e975c99a2e2648f9f406ee003
Liam Slade <liamslade2@gmail.com>
2025-07-21 20:26:44 -0400

    Add Operators project and implement arithmetic operations

---
+++ Fundamentals/Operators/operators.cs
@@ -0,0 +1,24 @@
+//Arithmetic operators
+//Addition
+int sum = 5 + 5;
+Console.WriteLine($"Addition: 5 + 5 = {sum}");
+//Subtraction
+int diff = 10 - 2;
+Console.WriteLine($"Subtraction: 10 - 2 = {diff}");
+//Multiplication
+int mult = 3 * 3;
+Console.WriteLine($"Mulitplication: 3 * 3 = {mult}");
+//Division
+int div = 20 / 4;
+Console.WriteLine($"Division: 20 / 4 = {div}");
+//Modulus
+int mod = 10 % 2;
+Console.WriteLine($"Modulus: 10 % 2 = {mod}");
+//Increment and Decrement 
+int x = 1;
+Console.WriteLine($"Starting Value: {x}");
+x++;
+Console.WriteLine($"Increment: {x}");
+x--;
+Console.WriteLine($"Decrement: {x}");
+
